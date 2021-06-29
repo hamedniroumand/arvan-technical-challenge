5fa4295f9a37a3e818df546209df837a8081763d
hamedniroumand <hamedniroomand@yahoo.com>
2021-06-29 21:42:28 +0430

    add fixed navbar and improve seo

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,97 @@
-console.log("global js file");
+var expandedIndex = [];
+let oldScroll = 0;
+
+Array.from(document.querySelectorAll('.expand-description')).forEach((element, index) => {
+    element.addEventListener("click", (event) => {
+        const parentNode = event.target.closest('.project-item');
+        if (!parentNode) return;
+
+        const content = parentNode.querySelector('.project-item__content');
+        if (!content) return;
+
+        parentNode.querySelector('span.expand-description').classList.toggle('active');
+
+        if (!parentNode.classList.contains('active')) {
+            expandedIndex.push(index)
+            content.style.maxHeight = "3000px";
+            parentNode.classList.add('active');
+        } else {
+            expandedIndex = expandedIndex.filter((i) => i != index - 1);
+            content.style.maxHeight = "0";
+            setTimeout(() => {
+                parentNode.classList.remove('active');
+            }, 300)
+        }
+    });
+});
+
+Array.from(document.querySelectorAll(".tab")).forEach(element => {
+    element.addEventListener("click", (event) => {
+        const id = element.getAttribute("data-id");
+        const type = element.getAttribute("data-type");
+        const target = document.querySelector(`[data-target='#${id}'][data-type='${type}']`);
+        const targetFamily = Array.from(document.querySelectorAll(`.tab-target[data-type='${type}']`))
+
+        removeActiveClassFromTabFamily(type)
+        event.target.closest('.tab').classList.add("active");
+
+        targetFamily.forEach(family => {
+            family.style.display = "none"
+        });
+        target.classList.add("active")
+        target.style.display = "block"
+    });
+});
+
+document.addEventListener("click", () => {
+    document.querySelectorAll(".custom-select").forEach(customSelect => {
+        customSelect.classList.remove("active")
+    });
+})
+
+Array.from(document.querySelectorAll('.custom-select__item input')).forEach(element => {
+    element.addEventListener('change', (e) => {
+        e.target.closest('.custom-select').querySelector("p").innerHTML = e.target.nextSibling.nextElementSibling.innerHTML;
+        e.target.closest('.custom-select').classList.remove('active')
+    });
+});
+
+document.querySelectorAll(".custom-select").forEach(customSelect => {
+    customSelect.addEventListener("click", (event) => {
+        event.stopPropagation();
+        if(!event.target.classList.contains('custom-select__item')) {
+            event.target.closest(".custom-select").classList.toggle("active");
+        }
+    });
+});
+
+Array.from(document.querySelectorAll(".custom-file")).forEach(label => {
+    label.nextSibling.nextElementSibling.addEventListener("change", event => {
+        label.innerHTML = event.target.files[0].name;
+        label.classList.add("selected")
+    })
+})
+
+const removeActiveClassFromTabFamily = (type) => {
+    Array.from(document.querySelectorAll(`.tab[data-type='${type}']`)).forEach(item => {
+        item.classList.remove('active')
+    });
+}
+
+const onDocumentScrolled = () => {
+    const header = document.querySelector(".main-header");
+    const navbar = document.querySelector(".main-nav");
+    const headerHeight = header.clientHeight;
+    const navHeight = navbar.clientHeight;
+    const topOfWindow = window.scrollY;
+    // console.log({header, navbar, navbar})
+    if (topOfWindow > headerHeight) {
+        if(!navbar.classList.contains("fixed")) navbar.classList.add("fixed")
+    } else {
+        if(topOfWindow < navHeight) {
+            if(navbar.classList.contains("fixed")) navbar.classList.remove("fixed")
+        }
+    }
+}
+
+document.addEventListener("scroll", onDocumentScrolled)
--- src/resources/js/home-page.js
+++ src/resources/js/home-page.js
@@ -8,7 +8,8 @@ const currentWindowWidth = window.innerWidth;
 
 responsiveMode(currentWindowWidth);
 
-document.addEventListener( 'DOMContentLoaded', function () {
+
+const onPageLoaded = () => {
     if(currentWindowWidth < breakPoints.sm) {
         document.querySelector(".projects .tabs").style.display = "none"
         document.querySelector(".project-tabs-mobile").style.display = "block"
@@ -39,8 +40,9 @@ document.addEventListener( 'DOMContentLoaded', function () {
             pagination: false,
         }).mount();
     }
+}
 
-});
+document.addEventListener( 'DOMContentLoaded', onPageLoaded);
 
 window.addEventListener("resize", (e) => {
     responsiveMode(window.innerWidth)
@@ -64,82 +66,4 @@ header.addEventListener("mousemove", (event) => {
     back.style.backgroundPositionX = `${finalX}px`;
 });
 
-var expandedIndex = [];
-
-Array.from(document.querySelectorAll('.expand-description')).forEach((element, index) => {
-    element.addEventListener("click", (event) => {
-        const parentNode = event.target.closest('.project-item');
-        if (!parentNode) return;
-
-        const content = parentNode.querySelector('.project-item__content');
-        if (!content) return;
-
-        parentNode.querySelector('span.expand-description').classList.toggle('active');
-
-        if (!parentNode.classList.contains('active')) {
-            expandedIndex.push(index)
-            content.style.maxHeight = "3000px";
-            parentNode.classList.add('active');
-        } else {
-            expandedIndex = expandedIndex.filter((i) => i != index - 1);
-            content.style.maxHeight = "0";
-            setTimeout(() => {
-                parentNode.classList.remove('active');
-            }, 300)
-        }
-    });
-});
-
-Array.from(document.querySelectorAll(".tab")).forEach(element => {
-    element.addEventListener("click", (event) => {
-        const id = element.getAttribute("data-id");
-        const type = element.getAttribute("data-type");
-        const target = document.querySelector(`[data-target='#${id}'][data-type='${type}']`);
-        const targetFamily = Array.from(document.querySelectorAll(`.tab-target[data-type='${type}']`))
-
-        removeActiveClassFromTabFamily(type)
-        event.target.closest('.tab').classList.add("active");
-
-        targetFamily.forEach(family => {
-            family.style.display = "none"
-        });
-        target.classList.add("active")
-        target.style.display = "block"
-    });
-});
-
-const removeActiveClassFromTabFamily = (type) => {
-    Array.from(document.querySelectorAll(`.tab[data-type='${type}']`)).forEach(item => {
-        item.classList.remove('active')
-    });
-}
-
-document.addEventListener("click", () => {
-    document.querySelectorAll(".custom-select").forEach(customSelect => {
-        customSelect.classList.remove("active")
-    });
-})
-
-Array.from(document.querySelectorAll('.custom-select__item input')).forEach(element => {
-    element.addEventListener('change', (e) => {
-        e.target.closest('.custom-select').querySelector("p").innerHTML = e.target.nextSibling.nextElementSibling.innerHTML;
-        e.target.closest('.custom-select').classList.remove('active')
-    });
-});
-
-document.querySelectorAll(".custom-select").forEach(customSelect => {
-    customSelect.addEventListener("click", (event) => {
-        event.stopPropagation();
-        if(!event.target.classList.contains('custom-select__item')) {
-            event.target.closest(".custom-select").classList.toggle("active");
-        }
-    });
-});
-
-Array.from(document.querySelectorAll(".custom-file")).forEach(label => {
-    label.nextSibling.nextElementSibling.addEventListener("change", event => {
-        label.innerHTML = event.target.files[0].name;
-        label.classList.add("selected")
-    })
-})
 
